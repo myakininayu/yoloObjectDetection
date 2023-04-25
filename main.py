580183a07d7517e5d33f49104000a65886a61a4d
Myakinina Yuliya <myakinina_yu@mail.ru>
2023-04-25 18:44:37 +0300

    Handling input parameters, creating output filename

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import sys
+
+
 def main():
-    print("Start yoloObjectDetection project")
+    argv = len(sys.argv)
+    print("Total arguments passed:", argv)
+
+    if argv != 3:
+        print("Expected 3 args, got", argv)
+    else:
+        input_filename = sys.argv[1]
+        object_class = sys.argv[2]
+
+        output_filename = input_filename[:input_filename.rfind(".")] + "_" + object_class + input_filename[input_filename.rfind("."):]
+        print(output_filename)
 
 if __name__ == '__main__':
     main()
