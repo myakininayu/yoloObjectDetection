d28e56dee649befe05584f1aee395d87103d53c1
Myakinina Yuliya <myakinina_yu@mail.ru>
2023-04-25 21:33:50 +0300

    Adding object detection and cropping

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import torch
 import cv2
+import numpy as np
 
 
 def main():
@@ -18,6 +19,8 @@ def main():
         model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
         capture = cv2.VideoCapture(input_filename)
+        img_array = []
+        obj_crops_array = []
 
         if not capture.isOpened():
             print("Unable to open:" + input_filename)
@@ -27,9 +30,36 @@ def main():
             ret, img = capture.read()
 
             if ret:
-                print("Video detected...")
-                cv2.imshow("Video", img)
-                cv2.waitKey(1)
+                imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+                detections = model(imgRGB)
+
+                # if detections on current img found
+                if detections and len(detections.pred[0]) > 0:
+                    for i in range(len(detections.pred[0])):
+                        classe = detections.names[int(detections.pred[0][i][-1])]
+
+                        if classe == object_class:
+                            x1 = int(detections.pred[0][i][0])
+                            y1 = int(detections.pred[0][i][1])
+                            x2 = int(detections.pred[0][i][2])
+                            y2 = int(detections.pred[0][i][3])
+
+                            # crop found object
+                            crop_img = img[y1:y2, x1:x2]
+
+                            # shift cropped object
+                            height, width = img.shape[:2]
+                            translation_matrix = np.float32([[1, 0, x1], [0, 1, y1]])
+                            shift_img = cv2.warpAffine(crop_img, translation_matrix, (width, height))
+
+                            # save objects of current img
+                            obj_crops_array.append(shift_img)
+
+                            cv2.imshow("Video", shift_img)
+                            cv2.waitKey(1)
+
+
             else:
                 print("No video detected...")
                 break
